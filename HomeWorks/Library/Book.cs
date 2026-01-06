4bd3d2851274e197d838abc0dffbaac946943c5a
Denys Shaitan <schaitan.den@gmail.com>
2026-01-06 16:31:28 +0100

    Added project library

---
+++ HomeWorks/Library/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyExtraHomeWorks
+{
+    internal class Book
+    {
+        private string name;
+        private string author;
+        private int pages;
+
+        public Book(string name, string author, int pages)
+        {
+            this.name = name;
+            this.author = author;
+            this.pages = pages;
+        }
+
+        public string Name {  get { return name; } }
+        public string Author { get { return author; } }
+        public int Pages { get { return pages;} }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Назва книги" + name);
+            Console.WriteLine("Автор книги" + author);
+            Console.WriteLine("Кількість сторінок" + pages);
+        }
+    }
+
+
+    
+}
---
+++ HomeWorks/Library/Library.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyExtraHomeWorks
+{
+    internal class Library
+    {
+        private Book[] books;
+
+        public Library(Book[] books)
+        {
+            this.books = books;
+        }
+
+    }
+}
