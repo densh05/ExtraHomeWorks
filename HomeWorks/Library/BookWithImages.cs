1dce53881baaf08a5a50c98cc113982ecf0c210c
Denys Shaitan <schaitan.den@gmail.com>
2026-02-07 17:53:39 +0100

    Pushed lidrary code

--- HomeWorks/Library/BookWithImages.cs
+++ HomeWorks/Library/BookWithImages.cs
@@ -2,51 +2,17 @@
 
 namespace Library
 {
-     class BookWithImages : Book
+     class BookWithImages(string name, string author, PageWithImages[] pages) 
+         : Book(name, author, pages)
     {
-        private readonly string[] images; // чому ми робимо ці поля readonly?
-        private readonly int[] imagespages; // чому ми робимо ці поля readonly?
-
-        public BookWithImages(string name, string author, int pages, string[] images, int[] imagespages)
-            :base(name, author, pages)
-        {
-            this.images = images;
-            this.imagespages = imagespages;
-        }
-
-        public string[] CurrentImages { get; private set; }
 
         public required string TitleImage { get; init; }  
 
-        public override bool MoveNext()
-        {
-            if (base.MoveNext())
-            {
-                int imageCount = 0;
-                string[] foundImages = new string[2];
-
-                for (int i = 0; i < imagespages.Length && imageCount < 2; i++)
-                {
-                    if (imagespages[i] == CurrentPageNumber
-                        )
-                    {
-                        foundImages[imageCount++] = images[i];
-                    }
-                }
-
-                CurrentImages = foundImages;   
-                
-                return true;    
-            }
-
-            return false;   
-        }  
-        
         public override string Open()
         {
             string result = base.Open();
             result += $"\nОбкладинка книги: {TitleImage}";
-            result += $"Ця книга містить {imagespages.Length} сторінок з картинками.";
+            result += $"Ця книга містить {pages.Length} сторінок з картинками.";
             return result;
         }
     }
