cbe0c0c08e22e654a80909f41ee442ee043b08be
Denys Shaitan <schaitan.den@gmail.com>
2026-04-03 09:58:04 +0200

    Made a new code,Using Enum

---
+++ HomeWorks/Enum/ProgressType.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enum
+{
+    enum ProgressType { Started, InProgress, Suspended, Finished }
+}
---
+++ HomeWorks/Enum/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enum
+{
+    struct MyTask
+    {
+        public int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Description { get; set; }
+        public ProgressType Status { get; set; }
+    }
+}
