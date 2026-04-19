47bdb8e8bf80b7f6b83cf0a4b07baca1f2f4b531
Denys Shaitan <schaitan.den@gmail.com>
2026-04-19 20:12:53 +0200

    Used Dictionary and List,made a library and numbers less than zero

---
+++ HomeWorks/Generics/NegativeArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    class NegativeArray
+    {
+        public static int[] LessThanZero(int[] array)
+        {
+            List<int> negativeNumbers = new List<int>();
+
+            foreach(int number in array)
+            {
+                if(number < 0)
+                {
+                    negativeNumbers.Add(number);
+                }
+            }
+            return negativeNumbers.ToArray();
+        }
+    }
+}
---
+++ HomeWorks/Generics/Program.cs
@@ -0,0 +1,54 @@
+﻿using Generics;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.Unicode;
+
+        int[] array = { -5, 3, -2, 7, -1, 0, 4, -12, 5 };
+        int[] result = NegativeArray.LessThanZero(array);
+        foreach(int num in result)
+        {
+            Console.WriteLine(num);
+        }
+
+        Console.WriteLine(new string('-', 40));
+
+        Dictionary<string, string> dict = new Dictionary<string, string>();
+
+        dict.Add("Яблуко", "Apple");
+        dict.Add("Авто", "Car");
+        dict.Add("Дівчина", "Girl");
+        dict.Add("Дім", "House");
+        dict.Add("Кінь", "Horse");
+        dict.Add("Книга", "Book");
+        dict.Add("Документ", "Document");
+        dict.Add("Ключ", "Key");
+        dict.Add("Бібліотека", "Library");
+        dict.Add("Ліжко", "Bed");
+
+        while (true)
+        {
+            Console.WriteLine("Hi,write the word u want to translate");
+            string word = Console.ReadLine();
+
+            if (word != null && dict.ContainsValue(word))
+            {
+                foreach (var item in dict)
+                {
+                    if (item.Value == word)
+                    {
+                        Console.WriteLine($"The translation of {word} is {item.Key}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Unknown word!");
+            }
+        }
+
+    }
+}
