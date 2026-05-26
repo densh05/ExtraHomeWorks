c9a23d1f0c6ca38921afeedccaa19ff5ea59cf0e
Denys Shaitan <schaitan.den@gmail.com>
2026-05-26 18:34:53 +0200

    Events code

---
+++ HomeWorks/Events/Currency.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Events
+{
+    class Currency
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public decimal Rate { get; set; }
+    }
+}
---
+++ HomeWorks/Events/Program.cs
@@ -0,0 +1,30 @@
+﻿using Events;
+
+class Program
+{
+
+    private static void OnCurrencyRateChanged(object sender, EventArgs e)
+    {
+        var args = e as EventRateArgs; 
+
+        var euro = args?.EurRate;
+
+        Console.WriteLine(euro);
+
+        var currencies = CurrencyRates.GetCurrentRates();
+        foreach (var currency in currencies)
+        {
+            Console.WriteLine(currency.Name);
+            Console.WriteLine(currency.Rate);
+        }
+    }
+
+    static void Main()
+    {
+        CurrencyRates.RatesUpdated += OnCurrencyRateChanged;
+
+        CurrencyRates.SetRates(42.3m, 45.2m, 53.7m);
+
+        CurrencyRates.SetRates(43.3m, 46.2m, 55.7m);
+    }
+}
