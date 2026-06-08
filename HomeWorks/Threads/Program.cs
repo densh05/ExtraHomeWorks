79e8cac475f9e1bbf2af34a8bf2dd7529116ba08
Denys Shaitan <schaitan.den@gmail.com>
2026-06-08 18:59:24 +0200

    Made a new code about threads

---
+++ HomeWorks/Threads/Program.cs
@@ -0,0 +1,18 @@
+﻿using Threads;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        FactorialCalculation calc = new FactorialCalculation();
+
+        for (int i = 5; i <= 10; i++)
+        {
+            int number = i;
+            new Thread(() => calc.Calculate(number)).Start(); //Start second thread
+        }
+
+        calc.Calculate(25); //Start first thread 
+    }
+}
