6ef38e90eec47671dcf4f9bd54950e5c1a9e8d3e
Denys Shaitan <schaitan.den@gmail.com>
2026-08-21 12:20:11 +0200

    Made a new cobe about deposit calculator (Essential coursework)

---
+++ HomeWorks/DepositCalculator/DepositData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DepositCalculator
+{
+    public class DepositData
+    {
+        public int NumberOfMonths 
+        { 
+            get;
+            init => field = value < 1 || value > 12
+                ? throw new ArgumentOutOfRangeException(nameof(NumberOfMonths), "Number of months must be between 1 and 12 !") 
+                : value;
+        }
+        public decimal Deposit 
+        { 
+            get;
+            init => field = value < 100
+                ? throw new ArgumentOutOfRangeException(nameof(Deposit), "The Deposit must be more than 100 !")
+                : value;
+        }
+        public decimal Interest { get; init; }
+        public decimal Balance { get; init; }
+    }
+}
---
+++ HomeWorks/DepositCalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace DepositCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello! Welcome to deposit bank, enter the amount you want to deposit!");
+            decimal initialDeposit = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("And also select the number of months from 1 to 12!");
+            int months = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Select option 1 if you want a deposit with capitalization or option 2 without capitalization");
+            int withCapitalization = Convert.ToInt32(Console.ReadLine());
+
+            List<DepositData> results = MonthlyDepositCalculation.CalculateMonthlyResult(initialDeposit, months, withCapitalization);
+
+            decimal sum = results.Sum(x => x.Interest);
+            decimal avg = results.Average(x => x.Interest);
+
+            Console.WriteLine();
+
+            Console.WriteLine($"{"Month",-10} {"Deposit",-12} {"Interest",-12} {"Balance",-12}");
+            Console.WriteLine(new string('-', 45));
+
+            foreach (var item in results)
+            {
+                Console.WriteLine($"{item.NumberOfMonths, -10} " +
+                                  $"{item.Deposit, -12} " +
+                                  $"{item.Interest,-12:F2} " +
+                                  $"{item.Balance:F2}" );
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Total amount of accured interest for the period {sum:F2}");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Average monthly income {sum:F2}");
+        }
+    }
+}
